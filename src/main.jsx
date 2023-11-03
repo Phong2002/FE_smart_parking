2a85691b7d848b022b12b3fed690e5a0bdef29e2
phonglm <phonglm@phx-smartschool.com>
2023-11-04 01:58:13 +0700

    update : remove strict mode

--- src/main.jsx
+++ src/main.jsx
@@ -5,9 +5,7 @@ import { ThemeProvider } from "@material-tailwind/react";
 import App from "./App";
 
 ReactDOM.createRoot(document.getElementById("root")).render(
-  <React.StrictMode>
     <ThemeProvider>
             <App/>
     </ThemeProvider>
-  </React.StrictMode>,
 );
