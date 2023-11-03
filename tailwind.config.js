99bc00087b4f4ee8fac844b6e03ce5402589ca31
phonglm <phonglm@phx-smartschool.com>
2023-11-04 01:54:59 +0700

    add : color orange , blue - phenikaa

--- tailwind.config.js
+++ tailwind.config.js
@@ -4,6 +4,10 @@ export default withMT({
   content: [ "./src/**/*.{html,js,jsx,tsx,ts}"],
   theme: {
     extend: {},
+    colors:{
+      'blue-phenikaa': '#223671',
+      'orange-phenikaa': '#F26B25',
+    }
   },
   plugins: [],
 });
