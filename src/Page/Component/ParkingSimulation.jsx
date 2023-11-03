bff72a4dec4b3d9796f4bdea57ef864426411c13
phonglm <phonglm@phx-smartschool.com>
2023-11-04 02:11:23 +0700

    create : ParkingSimulation.jsx

---
+++ src/Page/Component/ParkingSimulation.jsx
@@ -0,0 +1,36 @@
+import { useState } from "react";
+import ParkingSpot from "./ParkingSpot";
+import road from "../../assets/images/road.jpg"
+import { Checkbox, Switch } from "@material-tailwind/react";
+export default function ParkingSimulation(props) {
+
+  const parkingSpot = []
+  const [edit, setEdit] = useState(false)
+  for (let i = 0; i <= Math.ceil(props.total / props.rows); i++) {
+    let rowParkingSpot = []
+    for (let j = 1; j <= props.rows; j++) {
+      if ((i * props.rows) + j > props.total) {
+        break
+      }
+      rowParkingSpot.push(<div key={(i * props.rows) + j} 
+       className={(j >= 1 && j < props.rows) ? "mr-16 inline" : "inline"}>
+        <ParkingSpot  demo={props.demo} handleChangeState={props.handleChangeState} 
+        edit={edit} key={(i * props.rows) + j} index={(i * props.rows) + j} 
+        location={props.listLocation?props.listLocation[(i * props.rows) + j - 1]:{}} >
+          </ParkingSpot></div>);
+    }
+    parkingSpot.push(<div key={i} className="flex flex-row">{rowParkingSpot}</div>)
+  }
+  return (
+    <div >
+        <Checkbox id="editfloor-location" label="Chỉnh sửa" ripple={edit} onClick={() => setEdit(!edit)} />
+      <div className="p-5">
+        <div>
+        </div>
+        <div className={"flex flex-col pt-[40px] justify-center w-full p-3 border-b-8 border-l-8 border-r-8 border-dashed border-yellow-500 "} style={{ backgroundImage: `url(${road})` }} >
+          {parkingSpot}
+        </div>
+      </div>
+    </div>
+  )
+}
