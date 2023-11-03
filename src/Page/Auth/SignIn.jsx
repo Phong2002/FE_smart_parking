3ebaac8927fe31c2109a4ff8b85de40b6e6d2df0
phonglm <phonglm@phx-smartschool.com>
2023-11-04 01:55:48 +0700

    update : handle logic signin

--- src/Page/Auth/SignIn.jsx
+++ src/Page/Auth/SignIn.jsx
@@ -1,9 +1,62 @@
 import {Button, Card, CardBody, CardFooter, CardHeader, Checkbox, Input, Typography} from "@material-tailwind/react";
 import logo from "../../assets/images/logo_with_name.png";
 import {useNavigate} from "react-router-dom";
+import { useState } from "react";
+import { LoginAction } from "../../infrastructure/AuthAPI";
+import { emailRegex } from "../../ultis/regex";
 
 export default function SignIn() {
+    const [username ,setUserName] = useState('');
+    const [password,setPassword] = useState('');
+    const [mess,setMessage] = useState('');
+
     const navigate = useNavigate();
+
+
+    const validate = ()=>{
+        setMessage("")
+        if(!username){
+            setMessage("Tài khoản hoặc email không được để trống!")
+            return false
+        }
+        if(!password){
+            setMessage("Mật khẩu không được để trống!")
+            return false
+        }
+        return true
+    }
+
+    const handleLogin = ()=>{
+        if(!validate()){
+            return
+        }
+
+        let data ={}
+        if(emailRegex.test(username)){
+            data={
+                "email":username,
+                "password":password
+            }
+        }
+        else{
+            data={
+                "userName":username,
+                "password":password
+            } 
+        }
+
+        const action = {
+            success: function(){
+                navigate("/")
+            },
+            error: function(){
+                setMessage("Sai tài khoản hoặc mật khẩu")
+            }
+        }
+        LoginAction(data,action);
+    }
+
+
 
     document.title = 'Đăng nhập'
     return (
@@ -20,17 +73,32 @@ export default function SignIn() {
             <Typography variant="h3" color="deep-orange" className="pt-[100px] text-center">
                 Đăng nhập
             </Typography>
-            <CardBody className="flex flex-col gap-4">
-                <Input label="Email" size="lg"/>
-                <Input label="Password" size="lg"/>
-                <div className="-ml-2.5">
-                    <Typography className="text-center">
+            <CardBody className="flex flex-col gap-4 px-6 pt-6 pb-1">
+                <Input label="Tài khoản hoặc email" size="lg" value={username} onChange={e=>setUserName(e.target.value)} />
+                <Input label="Mật khẩu" size="lg" value={password} onChange={e=>setPassword(e.target.value)}  />
+                <div className="grid place-items-end">
+                    <Typography 
+                    color = "blue"
+                    className="text-center text-[14px] cursor-pointer select-none hover:text-blue-900"
+                    >
                         Quên mật khẩu
                     </Typography>
                 </div>
             </CardBody>
+            {mess?
+             <div className="grid place-items-center">
+             <Typography
+             color="red"
+             className="text-[15px]"
+             >
+                 {mess}
+             </Typography>
+         </div>
+         :
+         <div className="h-[22.5px]"></div>    
+            }
             <CardFooter className="pt-0">
-                <Button variant="gradient" fullWidth>
+                <Button variant="gradient" fullWidth onClick={handleLogin}>
                     Đăng nhập
                 </Button>
                 <Typography variant="small" className="mt-6 flex justify-center">
